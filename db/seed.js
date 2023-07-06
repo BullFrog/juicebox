a1292eedfeecf29bc8852c55ebb7edcfc206d3e7
BullFrog <camroth2002@gmail.com>
2023-07-05 20:16:17 -0600

    finished
    fixed

--- db/seed.js
+++ db/seed.js
@@ -13,9 +13,8 @@ const {
 
 async function dropTables() {
   try {
-    console.log("Starting to drop tables...");
+    console.log("Starting to drop tables");
 
-    // have to make sure to drop in correct order
     await client.query(`
       DROP TABLE IF EXISTS post_tags;
       DROP TABLE IF EXISTS tags;
